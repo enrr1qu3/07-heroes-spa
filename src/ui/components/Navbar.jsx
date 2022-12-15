a71d0cd54671ea729c229cc7fba1da62db469736
enrique <enriquealejo28@gmail.com>
2022-12-15 13:15:00 -0600

    proteccion de rutas

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,9 +1,14 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/context/AuthContext';
 
 
 export const Navbar = () => {
-
+  
   const navigate = useNavigate();
+
+  const { user } = useContext(AuthContext)
+
 
   const onLogout = () => {
     navigate('/login', {
@@ -47,7 +52,7 @@ export const Navbar = () => {
       <div className='navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end'>
         <ul className='navbar-nav ml-auto'>
 
-          <span className='nav-item nav-link text-info'>Enrique</span>
+          <span className='nav-item nav-link text-info'>{user?.name}</span>
           <button className='nav-item nav-link btn'
            onClick={ onLogout } >Logout</button>
 
